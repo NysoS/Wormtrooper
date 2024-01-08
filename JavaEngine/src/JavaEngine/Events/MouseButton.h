f1d1dad13e031b4712324ae459f25fab465096df
NysoS <k.ledoux.dev@gmail.com>
2024-01-08 21:32:13 +0100

    #4 add & bind sfmlEvent with JavaEngine event

---
+++ JavaEngine/src/JavaEngine/Events/MouseButton.h
@@ -0,0 +1,18 @@
+#ifndef MOUSE_BUTTON_H
+#define MOUSE_BUTTON_H
+
+using MouseButton = uint16_t;
+
+namespace JavaEngine
+{
+	enum MouseButton
+	{
+        MouseLeft,   
+        MouseRight,  
+        MouseMiddle, 
+        MouseExtra1, 
+        MouseExtra2  
+	};
+}
+
+#endif

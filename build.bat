15cbf210dcbfbf11a88d606cb8ed4b31438f0597
NysoS <k.ledoux.dev@gmail.com>
2024-01-12 00:33:40 +0100

    #5 install vcpkg submodule

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+set currentDir=%cd%
 if exist build (goto :clean) else (goto :build)
 exit
 
@@ -7,11 +8,28 @@ echo -- Cleaning old files
 rmdir /s /q build
 goto :build
 
+:gitmodule
+echo -- Update submodule
+git submodule init
+git submodule update
+goto :vcpkg_install
+
+:vcpkg_install
+echo -- Install vcpkg
+cd vendor/vcpkg
+cmd /c "bootstrap-vcpkg.bat --disable-metrics"
+goto :vcpkg_module
+
+:vcpkg_module
+echo -- Install vcpkg modules
+cmd /c "vcpkg install"
+vcpkg integrate install
+cd %currentDir%
+goto :build
+
 :build
 mkdir build
 cd build
 cmake ..
 echo -- Build successful
 pause
-
-start Wormtrooper.sln

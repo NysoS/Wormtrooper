5999f09058463d0d890fa65ce16e538c39c380f9
NysoS <k.ledoux.dev@gmail.com>
2024-03-13 00:43:05 +0100

    add namespace & je_api

--- JavaEngine/src/JavaEngine/Front_End/UI/UIElements/UIImage.h
+++ JavaEngine/src/JavaEngine/Front_End/UI/UIElements/UIImage.h
@@ -1,33 +1,34 @@
 #pragma once
 
-#include "../UIElement.h"
+#include "JavaEngine/Front_End/UI/UIElement.h"
+#include "JavaEngine/Core/Core.h"
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/Color.hpp>
 
-
-class UIImage : public UIElement
+namespace JavaEngine
 {
-public:
+	class JE_API UIImage : public UIElement
+	{
+	public:
 
-	explicit UIImage(sf::Vector2f size);
-	UIImage(UIElement& parent, sf::Vector2f size, std::string& name);
+		explicit UIImage(sf::Vector2f size);
+		UIImage(UIElement& parent, sf::Vector2f size, std::string& name);
 
-	~UIImage() override;
+		~UIImage() override;
 
-	void SetPosition(float positionX, float positionY) override;
-	void SetScale(float scaleX, float scaleY) override;
-	void Draw(sf::RenderWindow& window) const override;
+		void SetPosition(float positionX, float positionY) override;
+		void SetScale(float scaleX, float scaleY) override;
+		void Draw(sf::RenderWindow& window) const override;
 
-	void SetTexture(sf::Texture& texture);
-	void SwitchTexture(sf::Texture& newTexture);
-	void SetColor(sf::Color& color);
+		void SetTexture(sf::Texture& texture);
+		void SwitchTexture(sf::Texture& newTexture);
+		void SetColor(sf::Color& color);
 
-	sf::Vector2f GetSpriteSize() const;
+		sf::Vector2f GetSpriteSize() const;
 
+	private:
+		sf::Sprite Sprite;
+		sf::Color ImageColor;
 
-
-private:
-	sf::Sprite Sprite;
-	sf::Color ImageColor;
-
-};
+	};
+}
--- JavaEngine/src/JavaEngine/Front_End/UI/UIElements/UIText.h
+++ JavaEngine/src/JavaEngine/Front_End/UI/UIElements/UIText.h
@@ -1,25 +1,29 @@
 #pragma once
 
-#include "../UIElement.h"
+#include "jepch.h"
+#include "JavaEngine/Core/Core.h"
+#include "JavaEngine/Front_End/UI/UIElement.h"
 #include <SFML/Graphics/Text.hpp>
-#include <string>
 
-class UIText : public UIElement
+namespace JavaEngine
 {
-	explicit UIText(sf::Vector2f size);
-	UIText(UIElement& parent, sf::Vector2f size, std::string& name);
+	class JE_API UIText : public UIElement
+	{
+		explicit UIText(sf::Vector2f size);
+		UIText(UIElement& parent, sf::Vector2f size, std::string& name);
 
-	~UIText() override;
+		~UIText() override;
 
-	void Draw(sf::RenderWindow& window) const override;
-	void SetPosition(float positionX, float positionY) override;
-	void SetScale(float scaleX, float scaleY) override;
+		void Draw(sf::RenderWindow& window) const override;
+		void SetPosition(float positionX, float positionY) override;
+		void SetScale(float scaleX, float scaleY) override;
 
-	void SetString(std::string& text);
-	void SetFont(sf::Font& font);
-	void SetFontSize(float fontSize);
+		void SetString(std::string& text);
+		void SetFont(sf::Font& font);
+		void SetFontSize(float fontSize);
 
-protected:
-	sf::Text Text;
+	protected:
+		sf::Text Text;
 
-};
+	};
+}

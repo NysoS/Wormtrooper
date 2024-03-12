fc46ccda71d97c3f8d04a8885100b748df7fba25
NysoS <k.ledoux.dev@gmail.com>
2024-03-13 00:43:05 +0100

    add namespace & je_api

--- JavaEngine/src/JavaEngine/Front_End/UI/UIElement.h
+++ JavaEngine/src/JavaEngine/Front_End/UI/UIElement.h
@@ -1,26 +1,29 @@
 #pragma once
+
+#include "JavaEngine/Core/Core.h"
+
 #include <SFML/Graphics.hpp>
 
-class UIElement
+namespace JavaEngine
 {
-public:
-	sf::Vector2f Size;
-	sf::Vector2f Position;
-	sf::Vector2f Scale;
+	class JE_API UIElement
+	{
+	public:
+		sf::Vector2f Size;
+		sf::Vector2f Position;
+		sf::Vector2f Scale;
 
-	UIElement* ParentElement = nullptr;
-	std::string Name;
+		UIElement* ParentElement = nullptr;
+		std::string Name;
 
-	explicit UIElement(sf::Vector2f size);
-	UIElement(UIElement& parent, sf::Vector2f size, std::string& name);
-	virtual ~UIElement();
+		explicit UIElement(sf::Vector2f size);
+		UIElement(UIElement& parent, sf::Vector2f size, std::string& name);
+		virtual ~UIElement();
 
-	virtual void Draw(sf::RenderWindow& window) const = 0;
+		virtual void Draw(sf::RenderWindow& window) const = 0;
 
-	virtual void SetParent(UIElement& parent);
-	virtual void SetPosition(float positionX, float positionY);
-	virtual void SetScale(float scaleX, float scaleY);
-
-
-};
-
+		virtual void SetParent(UIElement& parent);
+		virtual void SetPosition(float positionX, float positionY);
+		virtual void SetScale(float scaleX, float scaleY);
+	};
+}
--- JavaEngine/src/JavaEngine/Front_End/UI/UIElements/UIWidget.cpp
+++ JavaEngine/src/JavaEngine/Front_End/UI/UIElements/UIWidget.cpp
@@ -1,21 +1,23 @@
 #include "UIWidget.h"
 
-
-UIWidget::UIWidget(UIElement& parent, sf::Vector2f size, std::string& name) : UIElement(parent, size, name)
+namespace JavaEngine
 {
+	UIWidget::UIWidget(UIElement& parent, sf::Vector2f size, std::string& name) : UIElement(parent, size, name)
+	{
 
-}
-
-UIWidget::~UIWidget()
-{
-	for (UIElement* element : ChildElements) {
-		//DELETE_PTR(element); delete all child
 	}
-}
 
-void UIWidget::Draw(sf::RenderWindow& window) const
-{
-	for (UIElement* element : ChildElements) {
-		element->Draw(window);
+	UIWidget::~UIWidget()
+	{
+		for (UIElement* element : ChildElements) {
+			//DELETE_PTR(element); delete all child
+		}
+	}
+
+	void UIWidget::Draw(sf::RenderWindow& window) const
+	{
+		for (UIElement* element : ChildElements) {
+			element->Draw(window);
+		}
 	}
 }
